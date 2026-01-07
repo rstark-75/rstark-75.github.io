8f92cfb7ddf37385f173b9ece427f3e01301cfcf
Rob <robertgstark@yahoo.co.uk>
2026-01-07 15:37:43 +0000

    Tidy Up

---
+++ gallery-data.js
@@ -0,0 +1,63 @@
+/* =====================
+   Gallery Data Collection
+   Edit ONLY this file
+===================== */
+
+const galleryCollection = [
+  {
+    src: "images/Kafka Hickstead.jpg",
+    caption: "Winning Hickstead Junior Champion 2001"
+  },
+  {
+    src: "images/Kafka.jpg",
+    caption: "Representing GB at Soley Farm CDIJG 2002"
+  },
+  {
+    src: "images/img020.jpg",
+    caption: "Reserve For The 2002 European Championships in Rome"
+  },
+  {
+    src: "images/Five Alive.jpg",
+    caption: "Winning CHAPS Non Native National Champion 2007"
+  },
+  {
+    src: "images/Lyla 2 Oct 2018.jpg",
+    caption: "Schooling Georgina & Bettina McLean's 4 year old mare Lyla, October 2018"
+  },
+  {
+    src: "images/dollar.jpg",
+    caption: "Schooling Judy Adams 7 year old Dollar May 2019"
+  },
+  {
+    src: "images/herb.jpg",
+    caption: "Schooling Nina Guy's 6 year old Herbie June 2019"
+  },
+  {
+    src: "images/dex.jpg",
+    caption: "Schooling Judy Adams 7 year old Dextro May 2019"
+  },
+  {
+    src: "images/img017.jpg",
+    caption: ""
+  },
+  {
+    src: "images/img016.jpg",
+    caption: ""
+  },
+  {
+    src: "images/img015.jpg",
+    caption: ""
+  },
+  {
+    src: "images/img018.jpg",
+    caption: ""
+  },
+  {
+    src: "images/tabi.jpg",
+    caption: "Robyn's youngest daughter Tabitha aged 1 riding her pony Honey Bun (a resue Minature Shetland)"
+  },
+  {
+    src: "images/chalky.jpg",
+    caption: "Robyn's eldest daughter Annabelle aged 6 riding her Welsh A pony Chalky"
+  }
+];
---
+++ testimonials-data.js
@@ -0,0 +1,22 @@
+/* =========================
+   Testimonials Collection
+   Replace text + image paths
+========================= */
+
+const testimonialsCollection = [
+  {
+    image: "images/georgina.png",
+    text: "I have known Robyn now for about 15 years, when she started teaching me at 6 years old, and where do I start! Robyn has just been the most amazing trainer any rider could ask for. She has the ability to see the smallest areas of concern and knows exactly how to correct them.  <BR><BR>For anyone looking for a trainer no matter what level Robyn is the one for you! She is not only such a wonderful teacher and rider, but also a lovely person and was always able to deal with my strops and frustrations as a teenager, to ensure I got the best outcome of every session. Robyn always keeps every lesson interesting and tailors each session to what you want to achieve with your horse. <BR><BR>Since having a break from riding while I was at university, I am very excited to have Robyn back to help bring on my new 3 year old, and can’t wait to see how she grows and improves through the levels with Robyn’s wonderful input.",
+    author: "Georgina McLean"
+  },
+  {
+    image: "images/placeholder.jpg",
+    text: "I first met Robyn 5 years ago when I was a livery at her yard in Churt. I have a 14.2 New Forest pony called Jester and I started having lessons with Robyn.<BR><BR>Needless to say I was a little bit apprehensive when I heard that she had represented Great Britain at dressage and had won championships with her wonder horse Kafka. Why would she want to teach us!! I needn’t have worried as she soon put us both at ease and I can honestly say we have never looked back. She explains things so well and always manages to solve any problems I have with Jester. Robyn gave us the incentive to try our hand at dressage albeit only Riding Club level which we really enjoyed and I have represented my local riding club Bookham many times. I would thoroughly recommend Robyn as a trainer whichever level you are riding at.",
+    author: "Client Name"
+  },
+  {
+    image: "images/placeholder.jpg",
+    text: "TESTIMONIAL TEXT GOES HERE – paste exact Wix testimonial text.",
+    author: "Client Name"
+  }
+];

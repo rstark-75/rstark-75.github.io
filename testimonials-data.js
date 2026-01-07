be683de78d04429fd1fdfad426f8edbd9e082146
Rob <robertgstark@yahoo.co.uk>
2026-01-07 17:34:20 +0000

    complete pages

--- testimonials-data.js
+++ testimonials-data.js
@@ -10,13 +10,34 @@ const testimonialsCollection = [
     author: "Georgina McLean"
   },
   {
-    image: "images/placeholder.jpg",
+    image: "images/margy.png",
     text: "I first met Robyn 5 years ago when I was a livery at her yard in Churt. I have a 14.2 New Forest pony called Jester and I started having lessons with Robyn.<BR><BR>Needless to say I was a little bit apprehensive when I heard that she had represented Great Britain at dressage and had won championships with her wonder horse Kafka. Why would she want to teach us!! I needn’t have worried as she soon put us both at ease and I can honestly say we have never looked back. She explains things so well and always manages to solve any problems I have with Jester. Robyn gave us the incentive to try our hand at dressage albeit only Riding Club level which we really enjoyed and I have represented my local riding club Bookham many times. I would thoroughly recommend Robyn as a trainer whichever level you are riding at.",
-    author: "Client Name"
+    author: "Margy Harvey"
   },
   {
-    image: "images/placeholder.jpg",
-    text: "TESTIMONIAL TEXT GOES HERE – paste exact Wix testimonial text.",
-    author: "Client Name"
+    image: "images/sarah.png",
+    text: "Robyn is a great teacher and has so much knowledge to share. She has helped bring my boy back to strength in a short period of time following a year off work. Robyn has a calm gentle approach and I come away from each lesson having leant so much. I want to ride at PSG and after each lesson I feel we are a step closer.",
+    author: "Sarah Harington"
+  },
+  {
+    image: "images/bettina.png",
+    text: "Robyn must be one of the most talented riders I have ever met.  She is intuitive with her knowledge of horses and instinctively knows the best course of action.<BR><BR>I first met Robyn when she was teaching my son and daughter, giving them a solid base.  We then embarked on buying our own horses.  Needless to say we wouldn’t have got through it without Robyn being on hand to assist and guide.  We trawled the length and breadth of the country and through Robyn’s expert eye bought two stunning ponies.  Under Robyn’s careful supervision these two ponies turned into superstars.  I will forever be grateful to Robyn for finding us our most perfect horses.  She later also helped with finding my daughter’s first horse when she was moving off ponies.  A difficult process at the best of times, but not when you have Robyn at your side!<BR><BR>I was lucky enough to take lessons with Robyn alongside the kids.  Having ridden myself since I was a child I have been taught by many different instructors, but none like Robyn.  Every lesson is well structured with a sense of purpose.  Robyn is always logical and one step ahead, creating sensible and achievable goals which naturally build on one another.  Robyn understands that horses, like riders, are individuals and structures her lessons accordingly.<BR><BR>I can’t recommend Robyn highly enough as a trainer for horse and rider.",
+    author: "Bettina McLean"
+  },
+  {
+    image: "images/nicki.png",
+    text: "Robyn is a fantastic instructor, calm and encouraging.  I thoroughly enjoy every lesson and always come away having learnt and achieved something.  Robyn has helped my horse and I improve so much and I know we will continue to do so..",
+    author: "Nicki Gabelle"
+  },
+  {
+    image: "images/katie.png",
+    text: "I am an adult riding a pony and after having a few instructors prior to Robyn, who I feel have not taken us seriously, I can safely say Robyn does not let our little stature lower her opinion of my rescue cob!<BR><BR>Having backed Billy myself Robyn has been invaluable in ironing out my bad habits and continuing to produce a well rounded, capable pony.",
+    author: "Katie Clarke"
+  },
+  {
+    image: "images/claire.png",
+    text: "A Great instructor, would highly recommend!.",
+    author: "Claire Edwards"
   }
+
 ];
